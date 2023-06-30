ef9724252ab49a0a6a6cbcff98115555824b44e6
Enoc Delgado <105158271+EnocDelgado@users.noreply.github.com>
2023-06-29 21:45:03 -0500

    Section 5 finished

--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -7,21 +7,11 @@ import { CreateCarDto, UpdateCarDto } from './dto/dto';
 export class CarsService {
 
     private cars: Car[] = [
-        {
-            id: uuid(),
-            brand: 'Toyota',
-            model: 'Corolla'
-        },
-        {
-            id: uuid(),
-            brand: 'Honda',
-            model: 'Civic'
-        },
-        {
-            id: uuid(),
-            brand: 'Jeep',
-            model: 'Cherokee'
-        }
+        // {
+        //     id: uuid(),
+        //     brand: 'Toyota',
+        //     model: 'Corolla'
+        // }
     ];
 
     findAll() {
@@ -74,6 +64,9 @@ export class CarsService {
         if ( car.id !== id ) throw new BadRequestException(`Car id ${ id } does not exist`)
 
         this.cars = this.cars.filter( car => car.id !== id );
+    }
 
+    fillCarsWithSeedData( cars: Car[] ) {
+        this.cars = cars;
     }
 }
